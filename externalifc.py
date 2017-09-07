c0ad1c8dbb14210ff3156ad37a31741d4d3c991a
Fredrik Andersson <fredrander@gmail.com>
2017-09-08 00:16:38 +0200

    Manual override

--- externalifc.py
+++ externalifc.py
@@ -4,21 +4,34 @@ import log
 import threading
 
 
-def init():
-    log.Add( log.LEVEL_DEBUG, "Start socket thread")
-    t = threading.Thread(target=_serverThread)
-    t.start()
+_cbOn = None
+_cbOff = None
+
+
+def init( cbOn, cbOff ):
+	log.Add( log.LEVEL_DEBUG, "Start socket thread")
+	global _cbOn
+	_cbOn = cbOn
+	global _cbOff
+	_cbOff = cbOff
+	t = threading.Thread(target=_serverThread)
+	t.start()
 
 
 def _handleReq( data ):
-    data = data.strip()
-    if data == "SWITCH=ON":
-        log.Add( log.LEVEL_INFO, "Switch on from external interface")
-    elif data == "SWITCH=OFF":
-        log.Add( log.LEVEL_INFO, "Switch off from external interface")
-    else:
-        log.Add( log.LEVEL_WARNING, "Unknown req. on external interface: {}".format(data) )
-    
+	data = data.strip()
+	if data == "SWITCH=ON":
+		log.Add( log.LEVEL_DEBUG, "Notify switch on from external interface")
+		if _cbOn:
+			_cbOn()
+	elif data == "SWITCH=OFF":
+		log.Add( log.LEVEL_DEBUG, "Notify switch off from external interface")
+		if _cbOff:
+			_cbOff()
+	else:
+		log.Add( log.LEVEL_WARNING, "Unknown req. on external interface: {}".format(data) )
+   
+ 
 def _serverThread():
     log.Add( log.LEVEL_DEBUG, "Setup server socket on {}:{}".format( settings.SOCKET_IP, settings.SOCKET_PORT ))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- main.py
+++ main.py
@@ -7,17 +7,35 @@ import sys
 import settings
 
 
+_manualOverride = False
+
+
+def _cbExtIfcOn():
+	global _manualOverride
+	_manualOverride = True
+	log.Add( log.LEVEL_INFO, "Manual switch ON" )
+	relay.setOn1()
+
+
+def _cbExtIfcOff():
+	global _manualOverride
+	_manualOverride = True
+	log.Add( log.LEVEL_INFO, "Manual switch OFF" )
+	relay.setOff1()
+
+
 def main():
-	externalifc.init()
+	externalifc.init( _cbExtIfcOn, _cbExtIfcOff )
 	relay.init()
 	relay.setOff2()
 	while True:
-		if timer.active():
-			log.Add( log.LEVEL_INFO, "Timer ON" )
-			relay.setOn1()
-		else:
-			log.Add( log.LEVEL_INFO, "Timer OFF" )
-			relay.setOff1()
+		if not _manualOverride:
+			if timer.active():
+				log.Add( log.LEVEL_INFO, "Timer ON" )
+				relay.setOn1()
+			else:
+				log.Add( log.LEVEL_INFO, "Timer OFF" )
+				relay.setOff1()
 		time.sleep(settings.UPDATE_INTERVAL_SEC)
 	relay.done()		
 	return 0
